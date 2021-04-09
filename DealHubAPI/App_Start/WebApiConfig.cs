58a5a1296a744d1302966151b5f1034c7732cef3
ankita <atulunitech@gmail.com>
2021-04-09 20:34:46 +0530

    commit friday last work

--- DealHubAPI/App_Start/WebApiConfig.cs
+++ DealHubAPI/App_Start/WebApiConfig.cs
@@ -12,17 +12,22 @@ namespace DealHubAPI
     {
         public static void Register(HttpConfiguration config)
         {
+            //config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            config.EnableCors();
+            var cors = new System.Web.Http.Cors.EnableCorsAttribute("*", "*", "*");
+            //config.EnableCors(cors);
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new Models.CustomLogDelegatHandler());
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
