a4df34de29ba0b94bb40ffdf7da099515a8aa5d9
shaileshn86 <naikshailu07@gmail.com>
2021-07-10 12:20:14 +0530

    change for comment master

---
+++ DealHub_Domain/Masters/MstCommentTypeParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealHub_Domain.Masters
+{
+    public class MstCommentTypeParameters:Mstcommonparameters
+    {
+        public int _comment_type_id { get; set; }
+
+        public string _comment_type { get; set; }
+    }
+}
---
+++ DealHub_Service/Implemantations/APIServices/MstCommentTypeService.cs
@@ -0,0 +1,24 @@
+﻿using DealHub_Domain.DashBoard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DealHub_Dal.Masters;
+using DealHub_Domain.Masters;
+
+namespace DealHub_Service.Implemantations.APIServices
+{
+    public class MstCommentTypeService
+    {
+        public static string GetMstCommentType(Mstcommonparameters model)
+        {
+            return MstCommentType.GetMstCommentType(model);
+        }
+
+        public static List<commanmessges> Update_Mst_CommentType(MstCommentTypeParameters model)
+        {
+            return MstCommentType.Update_Mst_CommentType(model);
+        }
+    }
+}
