c5b74f509a17bc474f9133e12fd9096657911a97
shaileshn86 <naikshailu07@gmail.com>
2021-07-20 19:03:07 +0530

    change for mst role api

---
+++ DealHub_Service/Implemantations/APIServices/MstRoleService.cs
@@ -0,0 +1,24 @@
+﻿using DealHub_Domain.Masters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DealHub_Dal.Masters;
+using DealHub_Domain.DashBoard;
+
+namespace DealHub_Service.Implemantations.APIServices
+{
+    public class MstRoleService
+    {
+        public static string GetMstRole(Mstcommonparameters model)
+        {
+            return MstRole.GetMstRole(model);
+        }
+
+        public static List<MstRoleDetailParameters> Update_Mst_Roles(MstRoleParameters model)
+        {
+            return MstRole.Update_Mst_Roles(model);
+        }
+    }
+}
